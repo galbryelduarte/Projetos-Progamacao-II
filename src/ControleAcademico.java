6116a0158b72395bb439272c7885023bc371e239
Gabryel Duarte <elielriven@gmail.com>
2026-09-23 16:15:51 -0300

    Terminei o programa

--- src/ControleAcademico.java
+++
@@ -1,8 +0,0 @@
-public class ControleAcademico {
-    public static void main(String[] args) {
-        Aluno a1 = new Aluno("JOAO", 1990);
-        Aluno a2 = new Aluno("MARIA", 2000);
-        System.out.println(a1);
-        System.out.println(a2);
-    }
-}
